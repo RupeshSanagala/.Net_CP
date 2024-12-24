20ef23be86183f2de29224ceec663572fdd503f0
RUPESH KUMAR REDDY SANAGALA <116737181+RupeshSanagala@users.noreply.github.com>
2024-12-24 17:39:43 +0530

    Add files via upload

---
+++ Interface1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsTasks
+{
+    internal interface Interface1
+    {
+        abstract void Drive();
+        abstract bool Refuel(int amountOfGas);
+    }
+    class Car : Interface1
+    {
+        int starting_fuel;
+
+        public Car(int starting_fuel)
+        {
+            this.starting_fuel = starting_fuel;
+        }
+
+        public void Drive()
+        {
+            if (starting_fuel > 0)
+            {
+                Console.WriteLine("Car is in Driving Mode");
+            }
+            else
+            {
+                Console.WriteLine("No fuel");
+            }
+        }
+
+        public bool Refuel(int amountOfGas)
+        {
+            starting_fuel += amountOfGas;
+            return true;
+        }
+    }
+}
