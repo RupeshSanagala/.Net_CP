3b1c319749e4d8fcf8922005930df20c796e5b7b
RUPESH KUMAR REDDY SANAGALA <116737181+RupeshSanagala@users.noreply.github.com>
2024-12-24 18:10:41 +0530

    Add files via upload

---
+++ OopsTasks/Animal.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsTasks
+{
+    abstract class Animal
+    {
+        string Name{get; set; }
+
+       public abstract void Eat();
+    }
+    class Dog: Animal { 
+       public override void Eat()
+        {
+            Console.WriteLine("dog is Eating");
+        }
+    }
+}
