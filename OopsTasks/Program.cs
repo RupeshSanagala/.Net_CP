d695bfae572f1a5530a784395769d050f2600283
RUPESH KUMAR REDDY SANAGALA <116737181+RupeshSanagala@users.noreply.github.com>
2024-12-24 17:42:15 +0530

    Update Program.cs

--- OopsTasks/Program.cs
+++ OopsTasks/Program.cs
@@ -6,7 +6,7 @@ namespace OopsTasks
     {
         private static void Main(string[] args)
         {
-
+              //PhotoBook.cs
                 PhotoBook p = new PhotoBook(16);
 
                 Console.WriteLine(p.GetNoOfPages());
@@ -19,7 +19,7 @@ namespace OopsTasks
                 BigPhotoBook b = new BigPhotoBook(64);
                 Console.WriteLine(b.GetNoOfPages1());
             
-
+             //person.cs
             int total = 3;
             person[] persons = new person[total];
 
@@ -47,7 +47,7 @@ namespace OopsTasks
                     ((Student)persons[i]).Study();
                 }
             }
-
+             //Shapes.cs
             Circle c1 = new Circle();
             Console.WriteLine("Area of circle is:"+ c1.Area(5));
             Console.WriteLine("Perimeter of circle is:" + c1.Perimeter(5));
@@ -56,6 +56,16 @@ namespace OopsTasks
 
             Console.WriteLine("Area of rectangle is: " + r1.Area(3, 3));
             Console.WriteLine("Perimeter of rectangle is: " + r1.Perimeter(3, 3));
+
+            //interface1.cs
+            Car c4 = new Car(0);
+           
+            Console.WriteLine("Enter the amount of refuel you want:");
+            int amountOfFuelNeed = int.Parse(Console.ReadLine());
+            if (c4.Refuel(amountOfFuelNeed))
+            {
+                c4.Drive();
+            }
 
         }
     }
