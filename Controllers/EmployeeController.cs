8acc2a425f8a76c59924a5ef536e811bad02a70a
RUPESH KUMAR REDDY SANAGALA <116737181+RupeshSanagala@users.noreply.github.com>
2025-01-03 16:57:04 +0530

    Add files via upload

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace MyDemoApplication.Models
+{
+    public class EmployeeController : Controller
+    {
+        private CompanyContext context;
+
+        public EmployeeController(CompanyContext cc)
+        {
+            this.context = cc;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            List<SelectListItem> dept = new List<SelectListItem>();
+            dept = context.Department.Select(x=>new SelectListItem
+            {
+                Text = x.Name,
+                Value = x.Id.ToString()
+            }).ToList();
+            ViewBag.Department=dept;
+            return View();
+        }
+    }
+}
